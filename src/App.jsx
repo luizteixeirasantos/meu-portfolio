eb8bc34cb7c23c72d04b73b87cf8964343b80484
Luiz <luiz.hh200@gmail.com>
2026-03-02 22:41:55 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import { motion } from "framer-motion";
 import { useState } from "react";
 import {
