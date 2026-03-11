036b10520cc46bba84b27358bc583cc9b2ef272f
Luiz <luiz.hh200@gmail.com>
2026-03-10 22:54:15 -0300

    colocando avatar

--- src/App.jsx
+++ src/App.jsx
@@ -50,55 +50,6 @@ const softSkills = [
 const meusProjetos = [
   {
     id: 1,
-    icone: <GiFarmTractor style={{ fontSize: "2rem", fill: "#00ff00" }} />,
-    titulo: "Fieldlytics",
-    descricao:
-      "Utilizamos Inteligência Artificial avançada para análise de imagens agrícolas e integração de dados climáticos em tempo real, permitindo diagnósticos precisos e recomendações personalizadas para cada tipo de plantação, além de controle financeiro da fazenda.",
-    detalhes: `Fieldlytics é uma plataforma SaaS (Software as a Service) voltada para o agronegócio, que utiliza Inteligência Artificial e dados climáticos em tempo real para auxiliar produtores rurais na tomada de decisões mais inteligentes, sustentáveis e produtivas.
-
-      O objetivo do projeto é revolucionar a agricultura por meio da tecnologia, tornando soluções avançadas acessíveis tanto para pequenos produtores quanto para grandes fazendas.`,
-    tecnologias: ["React", "Node.js", "Python"],
-    github: "https://github.com/luizteixeirasantos/meu-portfolio",
-    live: null,
-  },
-  {
-    id: 2,
-    icone: <FaDatabase style={{ fontSize: "2rem", fill: "#f97316" }} />,
-    titulo: "Sistema de OS",
-    descricao:
-      "Sistema web do tipo MES (Manufacturing Execution System) simplificado, focado no controle de chão de fábrica.",
-    detalhes: `Este projeto é um sistema web do tipo MES (Manufacturing Execution System) simplificado, focado no controle de chão de fábrica. Ele integra três setores principais em tempo real:
-
-    Produção (Operador): Registra início/fim de lotes, apontamento de produção hora-a-hora, paradas de máquina e solicita materiais.
-
-    Almoxarifado: Recebe pedidos de materiais em tempo real (via WebSocket) e gerencia o atendimento.
-
-    Administração: Gerencia usuários, permissões, catálogos e visualiza relatórios históricos de chamados e produção.`,
-    tecnologias: ["MySQL", "Node.js", "Backend"],
-    github: "https://github.com/luizteixeirasantos/sistema_os.git",
-    live: null,
-  },
-  {
-    id: 3,
-    icone: <FaCog style={{ fontSize: "2rem", fill: "#a855f7" }} />,
-    titulo: "Dashboard WaveSolder",
-    descricao:
-      "Sistema automático de calculo de fluxo, visando performance, qualidade e economia.",
-    detalhes: `Sistema criado para automatizar o cálculo do fluxo ideal para placas de circuito impresso (PCI) em linhas de produção. 
-
-    O sistema considera variáveis operacionais como:
-
-    - Tamanho da PCI
-    - Velocidade do transportador (conveyor)
-    - Especificações técnicas do fluxo
-
-`,
-    tecnologias: ["JavaScript", "Automação", "CSS"],
-    github: "https://github.com/luizteixeirasantos/DashboardWave.git",
-    live: null,
-  },
-  {
-    id: 4,
     icone: <FaHeartbeat style={{ fontSize: "2rem", fill: "#f80000" }} />,
     titulo: "Landing Page Nutricionista",
     descricao:
@@ -110,7 +61,7 @@ const meusProjetos = [
     live: "https://nutri-landing-page-blush.vercel.app/",
   },
   {
-    id: 5,
+    id: 2,
     icone: <AiFillCalculator style={{ fontSize: "3rem", fill: "#069db8" }} />,
     titulo: "Calculadora de Retroativo Salarial",
     descricao:
@@ -144,6 +95,55 @@ const meusProjetos = [
     github: "https://github.com/luizteixeirasantos/calculadora_retroativo.git",
     live: "https://calculadora-retroativo-salario.vercel.app/",
   },
+  {
+    id: 3,
+    icone: <GiFarmTractor style={{ fontSize: "2rem", fill: "#00ff00" }} />,
+    titulo: "Fieldlytics",
+    descricao:
+      "Utilizamos Inteligência Artificial avançada para análise de imagens agrícolas e integração de dados climáticos em tempo real, permitindo diagnósticos precisos e recomendações personalizadas para cada tipo de plantação, além de controle financeiro da fazenda.",
+    detalhes: `Fieldlytics é uma plataforma SaaS (Software as a Service) voltada para o agronegócio, que utiliza Inteligência Artificial e dados climáticos em tempo real para auxiliar produtores rurais na tomada de decisões mais inteligentes, sustentáveis e produtivas.
+
+      O objetivo do projeto é revolucionar a agricultura por meio da tecnologia, tornando soluções avançadas acessíveis tanto para pequenos produtores quanto para grandes fazendas.`,
+    tecnologias: ["React", "Node.js", "Python"],
+    github: "https://github.com/luizteixeirasantos/meu-portfolio",
+    live: null,
+  },
+  {
+    id: 4,
+    icone: <FaDatabase style={{ fontSize: "2rem", fill: "#f97316" }} />,
+    titulo: "Sistema de OS",
+    descricao:
+      "Sistema web do tipo MES (Manufacturing Execution System) simplificado, focado no controle de chão de fábrica.",
+    detalhes: `Este projeto é um sistema web do tipo MES (Manufacturing Execution System) simplificado, focado no controle de chão de fábrica. Ele integra três setores principais em tempo real:
+
+    Produção (Operador): Registra início/fim de lotes, apontamento de produção hora-a-hora, paradas de máquina e solicita materiais.
+
+    Almoxarifado: Recebe pedidos de materiais em tempo real (via WebSocket) e gerencia o atendimento.
+
+    Administração: Gerencia usuários, permissões, catálogos e visualiza relatórios históricos de chamados e produção.`,
+    tecnologias: ["MySQL", "Node.js", "Backend"],
+    github: "https://github.com/luizteixeirasantos/sistema_os.git",
+    live: null,
+  },
+  {
+    id: 5,
+    icone: <FaCog style={{ fontSize: "2rem", fill: "#a855f7" }} />,
+    titulo: "Dashboard WaveSolder",
+    descricao:
+      "Sistema automático de calculo de fluxo, visando performance, qualidade e economia.",
+    detalhes: `Sistema criado para automatizar o cálculo do fluxo ideal para placas de circuito impresso (PCI) em linhas de produção. 
+
+    O sistema considera variáveis operacionais como:
+
+    - Tamanho da PCI
+    - Velocidade do transportador (conveyor)
+    - Especificações técnicas do fluxo
+
+`,
+    tecnologias: ["JavaScript", "Automação", "CSS"],
+    github: "https://github.com/luizteixeirasantos/DashboardWave.git",
+    live: null,
+  },
 ];
 
 function App() {
@@ -156,7 +156,9 @@ function App() {
     <div className="portfolio-container dark-theme">
       {/* NAVEGAÇÃO */}
       <nav className="main-nav">
-        <h1 className="logo">LUIZ HENRIQUE</h1>
+        <h1 className="logo" style={{ cursor: "default" }}>
+          LUIZ HENRIQUE
+        </h1>
         <button className="mobile-menu-btn" onClick={toggleMenu}>
           {isMenuOpen ? <FaTimes /> : <FaBars />}
         </button>
@@ -209,12 +211,18 @@ function App() {
         >
           <img src={minhaFoto} alt="Luiz Henrique" className="profile-img" />
           <div className="hero-content">
-            <h1 className="hero-title">Olá, eu sou o Luiz Henrique</h1>
+            <h1 className="hero-title" style={{ cursor: "default" }}>
+              Olá, eu sou o Luiz Henrique
+            </h1>
             <h2 className="subtitle">
-              <span className="text-light-green">DESENVOLVEDOR</span>{" "}
-              <span className="text-purple">FULL STACK</span>
+              <span className="text-light-green" style={{ cursor: "default" }}>
+                DESENVOLVEDOR
+              </span>{" "}
+              <span className="text-purple" style={{ cursor: "default" }}>
+                FULL STACK
+              </span>
             </h2>
-            <p className="hero-text">
+            <p className="hero-text" style={{ cursor: "default" }}>
               Profissional de tecnologia com formação técnica e vocação para o
               desenvolvimento web. Especialista em criar interfaces com React e
               arquiteturas de servidor com Node.js.
@@ -231,16 +239,23 @@ function App() {
 
       {/* SEÇÃO DE SKILLS (CATEGORIZADA) */}
       <section className="skills-section" id="skills">
-        <h2 className="section-title text-center">SKILLS</h2>
+        <h2 className="section-title text-center" style={{ cursor: "default" }}>
+          SKILLS
+        </h2>
 
         <div className="skills-category">
-          <h3 className="skills-subtitle text-light-green">HARD SKILLS</h3>
+          <h3
+            className="skills-subtitle text-light-green"
+            style={{ cursor: "default" }}
+          >
+            HARD SKILLS
+          </h3>
           <div className="skills-grid">
             {hardSkills.map((hab, index) => (
               <motion.div
                 className="skill-pill"
                 key={index}
-                style={{ "--skill-color": hab.cor }}
+                style={{ "--skill-color": hab.cor, cursor: "default" }}
                 initial={{ opacity: 0, x: -20 }}
                 whileInView={{ opacity: 1, x: 0 }}
                 viewport={{ once: true }}
@@ -253,13 +268,18 @@ function App() {
         </div>
 
         <div className="skills-category">
-          <h3 className="skills-subtitle text-purple">SOFT SKILLS</h3>
+          <h3
+            className="skills-subtitle text-purple"
+            style={{ cursor: "default" }}
+          >
+            SOFT SKILLS
+          </h3>
           <div className="skills-grid">
             {softSkills.map((hab, index) => (
               <motion.div
                 className="skill-pill"
                 key={index}
-                style={{ "--skill-color": hab.cor }}
+                style={{ "--skill-color": hab.cor, cursor: "default" }}
                 initial={{ opacity: 0, x: 20 }}
                 whileInView={{ opacity: 1, x: 0 }}
                 viewport={{ once: true }}
@@ -274,7 +294,9 @@ function App() {
 
       {/* SEÇÃO DE PROJETOS (AGORA COM CARROSSEL) */}
       <section className="projects-section" id="projects">
-        <h2 className="section-title text-center">PROJECTS</h2>
+        <h2 className="section-title text-center" style={{ cursor: "default" }}>
+          PROJECTS
+        </h2>
         <div className="carousel-container">
           <Swiper
             modules={[Navigation, Pagination, Autoplay]}
@@ -331,7 +353,7 @@ function App() {
                     Saiba Mais
                   </button>
                 </div>
-              </SwiperSlide >
+              </SwiperSlide>
             ))}
           </Swiper>
         </div>
@@ -376,7 +398,11 @@ function App() {
                   rel="noreferrer"
                   className="btn-primary"
                 >
-                  <FaGithub className="FaGithub" style={{fill: "#fff", fontSize: "1.3rem"}} /> GitHub
+                  <FaGithub
+                    className="FaGithub"
+                    style={{ fill: "#fff", fontSize: "1.3rem" }}
+                  />{" "}
+                  GitHub
                 </a>
                 {selectedProject.live && (
                   <a
@@ -419,7 +445,9 @@ function App() {
             <FaEnvelope />
           </a>
         </div>
-        <p className="footer-bottom">Copyright © 2026 Luiz Henrique.</p>
+        <p className="footer-bottom" style={{ cursor: "default" }}>
+          Copyright © 2026 Luiz Henrique.
+        </p>
       </footer>
     </div>
   );
